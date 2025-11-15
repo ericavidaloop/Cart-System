2a1ae9bc865a6ae39b488ce99360262a03f97409
Erica <vidalerica39@gmail.com>
2025-11-15 16:09:43 +0800

    updated cart

---
+++ Vcart.js
@@ -0,0 +1,17 @@
+//select all products
+//Log all products
+
+const main = document.querySelector('#main');
+//console.log(main)
+
+const section = main.firstElementChild;
+
+const products = section.lastElementChild.children
+
+console.log(products);
+
+
+ 
+
+
+  
