095749b98230a07f643ea839e055e67be1f75bcb
Erica <vidalerica39@gmail.com>
2025-11-17 13:30:36 +0800

    Updated Vcart

--- Vcart.js
+++ Vcart.js
@@ -11,7 +11,27 @@ const products = section.lastElementChild.children
 console.log(products);
 
 
- 
+ //Get Cart
+const cart = main
+    .firstElementChild
+    .nextElementSibling
+    .lastElementChild;
+
+console.log(cart)
+
+//Step 2. Convert HTML Collections to Array
+const productArray = Array.from(products);
+
+productArray.forEach((p) =>{
+    p.addEventListener('click', () =>{
+        //step 4
+        const h5 = document.createElement('h5')
+        h5.innerText = p.firstElementChild.innerText;
+
+        //step 5
+        cart.appendChild(p);
+    })
+});
 
 
   
